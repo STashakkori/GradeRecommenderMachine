f58cd7b3ed94dc1f3b943fcbe7587ba8b7a069f5
sina <st79375@appstate.edu>
2015-05-13 15:12:25 -0400

    Changed the implementation after parsing to now create a map datastructure from the raw data instead of writing to .txt file. Serialization of datastructures and subsequent storage implemented as well.

--- CrossValRecommender.py
+++ CrossValRecommender.py
@@ -1,5 +1,9 @@
 __author__ = 'sina'
 
+import pprint
+import cPickle
+import timeit
+
 def parsecsv(filename):
     print "TESTINGGGGG %%%%%%%"
     with open(filename) as file:
@@ -7,23 +11,46 @@ def parsecsv(filename):
 
     pieces = []
 
-    #destfile = open("preprocessing/testfile.txt","w")
-    #destfile.write("TESTING 123")
-    #destfile.close()
-
     for line in data:
         pieces.append(line.split(','))
 
+    studentdictionary = {}
+    activitydictionary = {}
+
     for i in range(0,len(pieces)):
         for j in range(0,len(pieces[i])):
-            if(i == 0):
-                # Headers
-                print(pieces[i][j])
+            if(i > 0 and pieces[i][j].strip('\r').strip('\n')):
+                studentdictionary.setdefault(pieces[i][0],{})[pieces[0][j].strip('\r').strip('\n')] = pieces[i][j].strip('\r').strip('\n')
 
-            if(j == 0):
-                # DummyId's
-                print(pieces[i][j])
+    for i in range(0,len(pieces)):
+        for j in range(0,len(pieces[i])):
+            if(i > 0 and pieces[i][j].strip('\r').strip('\n')):
+                activitydictionary.setdefault(pieces[0][j].strip('\r').strip('\n'),{})[pieces[i][0]] = pieces[i][j].strip('\r').strip('\n')
+
+    studentstoragefilename = "preprocessing/studentdictionary.cpickle"
+    activitystoragefilename = "preprocessing/activitydictionary.cpickle"
+    file1 = open(studentstoragefilename, "wb")
+    file2 = open(activitystoragefilename, "wb")
+
+    cPickle.dump(studentdictionary,file1,protocol=2)
+    file1.close()
+    cPickle.dump(activitydictionary,file2,protocol=2)
+    file2.close()
+
+    testarray1 = load_pickle(studentstoragefilename)
+    testarray2 = load_pickle(activitystoragefilename)
+
+    pp = pprint.PrettyPrinter(indent = 3)
+    pp.pprint(testarray1)
+    print("END")
 
     file.close()
     return
+
+
+def load_pickle(filename):
+    f = open(filename,"rb")
+    p = cPickle.load(f)
+    f.close()
+    return(p)
 
