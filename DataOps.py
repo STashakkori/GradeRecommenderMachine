8336f819748bc9b49e588f2607608cfb1874b3d1
Sina <st79375@appstate.edu>
2015-05-20 01:13:19 -0400

    Calculate mean and mode. Median will be a bit tricky but that's coming up along with parametric distribution calculations.

--- DataOps.py
+++ DataOps.py
@@ -10,8 +10,9 @@ class loadergetter:
     studentdict = {}
     activitydict = {}
     gradelist = []
-    graderegex = re.compile('^((\*?[A-DFSIW][+-]?) ?)?(NG|XG|NR|WB|WF|AU|XP|XC|CV|CR|WC|IE)*$')
-    traditionalgraderegex = re.compile('^\*?[A-DFSIW][+-]?$')
+    graderegex = re.compile('^((\*?[A-DFSIWU][+-]?) ?)?(NG|XG|NR|WB|WF|AU|XP|XC|CV|CR|WC|IE)*$')
+    traditionalgraderegex = re.compile('^\*?[A-DFSIWU][+-]?$')
+    gradenumericsmap = {"A":4.0,"A-":3.7,"B+":3.3,"B":3.0,"B-":2.7,"C+":2.3, "C":2.0,"C-":1.7,"D+":1.3,"D":1.0,"D-":0.7,"F":0.0}
 
     def __init__(self):
         self.studentdict = self.load_json("preprocessing/studentdictionary.json")
@@ -64,7 +65,7 @@ class loadergetter:
 
     def countsmapper(self,list):
         from collections import OrderedDict
-        countmap = OrderedDict([("A",0),("A-",0),("B+",0),("B",0),("B-",0),("C+",0),("C",0),("C-",0),("D+",0),("D",0),("D-",0),("F",0),("I",0),("S",0),("W",0),("*F",0),("Other",0)])
+        countmap = OrderedDict([("A",0),("A-",0),("B+",0),("B",0),("B-",0),("C+",0),("C",0),("C-",0),("D+",0),("D",0),("D-",0),("F",0),("I",0),("S",0),("U",0),("W",0),("*F",0),("Other",0)])
         for item in list:
             if self.traditionalgraderegex.search(item):
                 countmap[item] = countmap.setdefault(item,0) + 1
@@ -82,4 +83,21 @@ class loadergetter:
         ymax=max(list.values()) + 1
         plt.ylim(0,ymax)
         plt.autoscale(enable=True,axis=u'x',tight=None)
+        plt.title("Letter Grade Histogram of All Students That Have Taken a CS Class")
         plt.show()
+
+    def calculatelettergrademean(self,list):
+        sum = 0.0
+        count = 0.0
+        for grade in list:
+            if grade in self.gradenumericsmap:
+                sum = sum + self.gradenumericsmap[grade]
+                count = count + 1.0
+        return sum/count
+
+    def calculatelettergrademedian(self,list):
+        return list[len(list)/2]
+
+    def calculatelettergrademode(self,countsmap):
+        import operator
+        return max(countsmap.iteritems(), key=operator.itemgetter(1))[0]
