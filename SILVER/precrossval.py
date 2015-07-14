7342d3959bbf9b6b75a288200e374f95cc7f8990
sina <st79375@appstate.edu>
2015-07-14 13:49:39 -0400

    Added one feature to precrossval where the function caller can specify the number of orders prior to the target course to remove instead of simply keeping all courses of orders less than the target.

--- SILVER/precrossval.py
+++ SILVER/precrossval.py
@@ -13,41 +13,45 @@ import csv
 import numpy
 import operator
 
-
-def main(target_course):
+"""
+    main
+"""
+def main(target, goback):
+    goback = int(goback)
     result = numpy.load("CSDataFile_ForParry_2014Nov26.csv.npz")
     data = result['data']
     orders = result['orders']
     activity_list = list(result['activity_list'])
     student_list = result['student_list']
-    target_column = activity_list.index(target_course)
-    print target_column
-    print student_list.shape
-    removestudent_index = numpy.isnan(data[:,target_column])
+    target_column = activity_list.index(target)
+    removestudent_index = numpy.isnan(data[:, target_column])
     student_list = list(student_list[~removestudent_index])
-    data = data[~removestudent_index,:]
-    orders = orders[~removestudent_index,:]
+    data = data[~removestudent_index, :]
+    orders = orders[~removestudent_index, :]
 
-    for i in range(0,data.shape[0]):
-        target_order = orders[i,target_column]
-        for j in range(0,data.shape[1]):
-            if orders[i,j] >= target_order:
-                data[i,j] = numpy.nan
+    for i in range(0, data.shape[0]):
+        target_order = int(orders[i, target_column])
+        newtarget_order = 0
+        if goback < target_order:
+            newtarget_order = target_order - goback
 
-    new_filename = "precrossval.npz"
+        for j in range(0, data.shape[1]):
+            if orders[i, j] >= newtarget_order:
+                data[i, j] = numpy.nan
+
+    new_filename = "precrossval_goback" + str(goback) + ".npz"
     numpy.savez_compressed(new_filename, data=data, activity_list=activity_list, student_list=student_list)
 
-
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         print "Incorrect parameters. Try again."
         exit(-1)
 
     try:
         t0 = time.time()
         target_course = sys.argv[1]
-        # main("CSDataFile_ForParry_2014Nov26.csv")
-        main(target_course)
+        courses_togoback = sys.argv[2]
+        main(target_course, courses_togoback)
         t1 = time.time()
         total_time = t1 - t0
         print(colored("csv2mat ~=> " + str(total_time) + " seconds.", "yellow"))
