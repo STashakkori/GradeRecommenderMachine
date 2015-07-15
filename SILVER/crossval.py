2e53b91b9ceb385ec55696069a5096086baf835a
sina <st79375@appstate.edu>
2015-07-15 19:02:52 -0400

    Not much to say I just had to rewrite hundreds of lines of code because I accidentally rm * in the wrong directory and didn't realize I had not committed in a few hours. Oh well glad that's over.

--- SILVER/crossval.py
+++ SILVER/crossval.py
@@ -7,74 +7,85 @@
 __author__ = 'sina'
 __project__ = 'STProject'
 
-from matplotlib import mlab
-import operator
-import colorama
 from termcolor import colored
-import cPickle
 import numpy
 import time
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-from collections import OrderedDict
-import math
 import sys
 import random
+import re
+import os
 
 """
     main - main method of the crossval program.
-        :param argv1: command line argument -> the course name of the target.
-        :type argv1: string
-        :param argv2: command line argument -> the prerequisite course to the target that we are interested in.
-        :type argv2: string
 """
-def main(argv1,argv2):
-    filename = argv1
-    k = int(argv2)
-    colorama.init(autoreset=True)
-    print(colored("crossval","blue"))
-    result = numpy.load(argv1)
+def main(argv1, argv2, argv3, argv4):
+    file_name = argv1
+    base_name = os.path.splitext(file_name)[0]
+    k_folds = int(argv2)
+    n_iterations = int(argv3)
+    target_course = argv4
+    if not os.path.exists("crossvalin/" + target_course):
+        os.makedirs("crossvalin/" + target_course)
+    directory_name = "crossvalin/" + target_course + "/" + re.search('_.*', base_name).group(0)
+    result = numpy.load(file_name)
     data = result['data']
-    activity_list = result['activity_list']
-    student_list = result['student_list']
-    kfold(data, k, activity_list, student_list)
+    activity_list = list(result['activity_list'])
+    student_list = list(result['student_list'])
+    target_course_column = activity_list.index(target_course)
+    iterations, groupings = n_in_k_fold(data, k_folds, n_iterations, target_course_column)
+    save_data(data, k_folds, n_iterations, target_course, directory_name, iterations, groupings, student_list,
+              activity_list)
+
+def remove_target(d, t, g, grp):
+    for index, group_assignment in enumerate(grp):
+        if group_assignment == g:
+            row_to_remove = index
+            d[row_to_remove, t] = numpy.nan
+    return d
 
 
-def kfold(data, k, activity_list, student_list):
-    data_copy = data.copy()
+def n_in_k_fold(data, k, n, t):
+    iterations = []
+    groupings = []
+
     # generate list of numbers 1 to k in repeating sequential order for every row in data
-    groupings = [(i % 10)+1 for i in range(0,len(data))]
-    random.shuffle(groupings)
+    grouping = [(i % k) + 1 for i in range(0, len(data))]
+    for iteration in range(0, n):
+        random.shuffle(grouping)
+        groupings.append(grouping)
+        folds = []
+        for group in range(1, k + 1):
+            f = remove_target(data.copy(), t, group, grouping)
+            folds.append(f)
+        iterations.append(list(folds))
+    return iterations, groupings
 
-    for group_number in range(1, k + 1):
-        temp_matrixlist = []
-        for index, group_assignment in enumerate(groupings):
-            if group_assignment == group_number:
-                row_toremove = index
-                data_copy[row_toremove, :] = numpy.nan
-
-        # for i in range(0, k):
-        #     temp_matrixlist.append(data_copy.copy())
-
-        new_filename = "crossvalin/crossvalinfold" + str(group_number) + ".npz"
-        numpy.savez_compressed(new_filename, *[temp_matrixlist[i] for i in range(0, k)], activity_list=activity_list, student_list=student_list, groupings=groupings)
+def save_data(data, k, n, t, directory_name, iterations, groupings, student_list, activity_list):
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
+    for i in range(0, n):
+        new_filename = directory_name + "_iter" + str(i) + ".npz"
+        numpy.savez_compressed(new_filename, activity_list=activity_list, student_list=student_list,
+                               groupings=groupings[i], **{"fold" + str(j) : value for j,value in
+                                                          enumerate(iterations[i])})
 
 if __name__ == "__main__":
-    usage = colored("crossval ==> ERROR --> Improper command line arguments ~~> Usage : python imputemat.py <matrix.npy> <ROWMEAN, COLMEAN, EIG, SVD, or ALS>","red")
-    if len(sys.argv) > 3:
+    usage = colored("crossval ==> ERROR --> Improper command line arguments ~~> Usage : python imputemat.py "
+                    "<matrix.npy> <ROWMEAN, COLMEAN, EIG, SVD, or ALS>","red")
+    if len(sys.argv) > 5:
         print usage
         exit(-1)
     try:
         t0 = time.time()
         # "precrossvalout/precrossval_goback0.npz"
-        inputmatrix_filename = sys.argv[1]
-        numberof_folds = sys.argv[2]
-        numberof_iterations = sys.argv[3]
-        target_course = sys.argv[3]
-        main(inputmatrix_filename, numberof_folds)
+        input_matrix_filename = sys.argv[1]
+        number_of_folds = sys.argv[2]
+        number_of_iterations = sys.argv[3]
+        target_course = sys.argv[4]
+        main(input_matrix_filename, number_of_folds, number_of_iterations, target_course)
         t1 = time.time()
-        totaltime = t1 - t0
-        print(colored("crossval ~=> " + str(totaltime) + " seconds.","yellow"))
+        total_time = t1 - t0
+        print(colored("crossval ~=> " + str(total_time) + " seconds.","yellow"))
     except IOError as e:
         print e.strerror
         print usage
--- SILVER/precrossval.py
+++ SILVER/precrossval.py
@@ -16,30 +16,30 @@ import operator
 """
     main
 """
-def main(target, goback):
-    goback = int(goback)
+def main(target, go_back):
+    go_back = int(go_back)
     result = numpy.load("CSDataFile_ForParry_2014Nov26.csv.npz")
     data = result['data']
     orders = result['orders']
     activity_list = list(result['activity_list'])
     student_list = result['student_list']
     target_column = activity_list.index(target)
-    removestudent_index = numpy.isnan(data[:, target_column])
-    student_list = list(student_list[~removestudent_index])
-    data = data[~removestudent_index, :]
-    orders = orders[~removestudent_index, :]
+    remove_student_index = numpy.isnan(data[:, target_column])
+    student_list = list(student_list[~remove_student_index])
+    data = data[~remove_student_index, :]
+    orders = orders[~remove_student_index, :]
 
     for i in range(0, data.shape[0]):
         target_order = int(orders[i, target_column])
-        newtarget_order = 0
-        if goback < target_order:
-            newtarget_order = target_order - goback
+        new_target_order = 0
+        if go_back < target_order:
+            new_target_order = target_order - go_back
 
         for j in range(0, data.shape[1]):
-            if orders[i, j] >= newtarget_order:
+            if orders[i, j] > new_target_order:
                 data[i, j] = numpy.nan
 
-    new_filename = "precrossvalout/precrossval_goback" + str(goback) + ".npz"
+    new_filename = "precrossvalout/precrossval_goback" + str(go_back) + ".npz"
     numpy.savez_compressed(new_filename, data=data, activity_list=activity_list, student_list=student_list)
 
 if __name__ == "__main__":
